a94f14a46ab8e99fb56d190ec08d237df3d34e20
ziaulkarimsiam <150989078+ziaulkarimsiam@users.noreply.github.com>
2023-11-17 22:43:00 +0600

    CGPA.c

--- CGPA.c
+++ CGPA.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 main()
 {
-    float gpa[20], sum=0; int i,  courses;
+    float a[20], sum=0; 
+    int i,  n;
 
-    printf("Number's of Course: "); // value of n
-    scanf("%d", &courses); // value of the course
+    printf("Number's of Course: "); 
+    scanf("%d", &n); 
 
-    for (i=0; i<courses; i++)
+    for (i=0; i<n; i++)
     {
         printf("Enter GPA of Course %d: ", i+1);
-        scanf("%f", &gpa[i]); //array value
-        sum = sum + gpa[i]; // total value
+        scanf("%f", &a[i]); 
+        sum = sum + a[i]; 
     }
-    printf("Average CGPA is: %.2f", sum/(float)courses); // avg result
+    printf("Average CGPA is: %.2f", sum/(float)n); 
 }
